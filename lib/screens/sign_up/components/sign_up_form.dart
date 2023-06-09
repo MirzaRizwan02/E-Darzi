175205c494102c9df22c7737e2c406d9f43675d4
Mirza Rizwan <Mirza.rizwan0311@gmail.com>
2023-06-09 18:02:30 +0500

    INTEGRATING SIGNUP API

--- lib/screens/sign_up/components/sign_up_form.dart
+++ lib/screens/sign_up/components/sign_up_form.dart
@@ -1,11 +1,248 @@
+// import 'package:flutter/material.dart';
+// import '../../../components/custom_surfix_icon.dart';
+// import '../../../components/default_button.dart';
+// import '../../../components/form_error.dart';
+// import '../../../constants.dart';
+// import '../../../size_config.dart';
+// import 'package:http/http.dart' as http;
+//
+//
+// class SignUpForm extends StatefulWidget {
+//   @override
+//   _SignUpFormState createState() => _SignUpFormState();
+// }
+//
+// class _SignUpFormState extends State<SignUpForm> {
+//   final _formKey = GlobalKey<FormState>();
+//   String? Name;
+//   String? Email;
+//   String? number;
+//   bool remember = false;
+//   final List<String?> errors = [];
+//
+//   void addError({String? error}) {
+//     if (!errors.contains(error))
+//       setState(() {
+//         errors.add(error);
+//       });
+//   }
+//
+//   void removeError({String? error}) {
+//     if (errors.contains(error))
+//       setState(() {
+//         errors.remove(error);
+//       });
+//   }
+//
+//
+//   Future<void> handleSignup() async {
+//     try {
+//       final response = await http.post(
+//         Uri.parse('http://172.26.192.1:3000/signup'),
+//         body: {
+//           'name': Name,
+//           'email': Email,
+//           'phone': number,
+//         },
+//       );
+//
+//       if (response.statusCode == 200) {
+//         // Signup successful, show the welcome message
+//         showDialog(
+//           context: context,
+//           builder: (BuildContext context) {
+//             return AlertDialog(
+//               title: Text('Success'),
+//               content: Text('Signup successful. Welcome email sent!'),
+//               actions: [
+//                 TextButton(
+//                   onPressed: () {
+//                     Navigator.pop(context);
+//                   },
+//                   child: Text('OK'),
+//                 ),
+//               ],
+//             );
+//           },
+//         );
+//       } else {
+//         // Signup failed, handle the error
+//         showDialog(
+//           context: context,
+//           builder: (BuildContext context) {
+//             return AlertDialog(
+//               title: Text('Error'),
+//               content: Text(response.body),
+//               actions: [
+//                 TextButton(
+//                   onPressed: () {
+//                     Navigator.pop(context);
+//                   },
+//                   child: Text('OK'),
+//                 ),
+//               ],
+//             );
+//           },
+//         );
+//       }
+//     } catch (error) {
+//       // Handle any network or server errors
+//       showDialog(
+//         context: context,
+//         builder: (BuildContext context) {
+//           return AlertDialog(
+//             title: Text('Error'),
+//             content: Text('Signup error: $error'),
+//             actions: [
+//               TextButton(
+//                 onPressed: () {
+//                   Navigator.pop(context);
+//                 },
+//                 child: Text('OK'),
+//               ),
+//             ],
+//           );
+//         },
+//       );
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Form(
+//
+//       key: _formKey,
+//       child: Column(
+//         children: [
+//           SizedBox(height: getProportionateScreenHeight(20)),
+//           buildNameFormField(),
+//           SizedBox(height: getProportionateScreenHeight(20)),
+//           buildEmailFormField(),
+//           SizedBox(height: getProportionateScreenHeight(20)),
+//           buildNumberFormField(),
+//           FormError(errors: errors),
+//           SizedBox(height: getProportionateScreenHeight(20)),
+//           DefaultButton(
+//             text: "SIGNUP",
+//             press: () {
+//               if (_formKey.currentState!.validate()) {
+//                 _formKey.currentState!.save();
+//                 handleSignup();
+//
+//                 // if all are valid then go to success screen
+//                 // Navigator.pushNamed(context, CompleteProfileScreen.routeName);
+//               }
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//
+//   TextFormField buildNameFormField() {
+//     return TextFormField(
+//       keyboardType: TextInputType.name,
+//       onSaved: (newValue) => Name = newValue,
+//
+//       decoration: InputDecoration(
+//
+//         hintText: "Enter Your Name",
+//         border: OutlineInputBorder(
+//           borderRadius: BorderRadius.circular(5.0),
+//         ),
+//         contentPadding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
+//         // If  you are using latest version of flutter then lable text and hint text shown like this
+//         // if you r using flutter less then 1.20.* then maybe this is not working properly
+//         floatingLabelBehavior: FloatingLabelBehavior.always,
+//         suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Name.svg"),
+//       ),
+//     );
+//   }
+//
+//   TextFormField buildEmailFormField() {
+//     return TextFormField(
+//       keyboardType: TextInputType.emailAddress,
+//       onSaved: (newValue) => Email = newValue,
+//       onChanged: (value) {
+//         if (value.isNotEmpty) {
+//           removeError(error: kEmailNullError);
+//         } else if (emailValidatorRegExp.hasMatch(value)) {
+//           removeError(error: kInvalidEmailError);
+//         }
+//         return null;
+//       },
+//       validator: (value) {
+//         if (value!.isEmpty) {
+//           addError(error: kEmailNullError);
+//           return "";
+//         } else if (!emailValidatorRegExp.hasMatch(value)) {
+//           addError(error: kInvalidEmailError);
+//           return "";
+//         }
+//         return null;
+//       },
+//       decoration: InputDecoration(
+//
+//         hintText: "Enter Your Email Address",
+//         border: OutlineInputBorder(
+//           borderRadius: BorderRadius.circular(5.0),
+//         ),
+//         contentPadding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
+//         // If  you are using latest version of flutter then lable text and hint text shown like this
+//         // if you r using flutter less then 1.20.* then maybe this is not working properly
+//         floatingLabelBehavior: FloatingLabelBehavior.always,
+//         suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+//       ),
+//     );
+//   }
+//
+//   TextFormField buildNumberFormField() {
+//     return TextFormField(
+//       keyboardType: TextInputType.number,
+//       onSaved: (newValue) => number = newValue,
+//       onChanged: (value) {
+//         if (value.isNotEmpty) {
+//           removeError(error: kEmailNullError);
+//         } else if (phoneValidatorRegExp.hasMatch(value)) {
+//           removeError(error: kInvalidPhoneError);
+//         }
+//         return null;
+//       },
+//       validator: (value) {
+//         if (value!.isEmpty) {
+//           addError(error: kPhoneNumberNullError);
+//           return "";
+//         } else if (!phoneValidatorRegExp.hasMatch(value)) {
+//           addError(error: kInvalidPhoneError);
+//           return "";
+//         }
+//         return null;
+//       },
+//       decoration: InputDecoration(
+//
+//         hintText: "Enter Your Phone Number",
+//         border: OutlineInputBorder(
+//           borderRadius: BorderRadius.circular(5.0),
+//         ),
+//         contentPadding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
+//         // If  you are using latest version of flutter then lable text and hint text shown like this
+//         // if you r using flutter less then 1.20.* then maybe this is not working properly
+//         floatingLabelBehavior: FloatingLabelBehavior.always,
+//         suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+//       ),
+//     );
+//   }
+// }
+//
+
 import 'package:flutter/material.dart';
 import '../../../components/custom_surfix_icon.dart';
 import '../../../components/default_button.dart';
 import '../../../components/form_error.dart';
-
 import '../../../constants.dart';
 import '../../../size_config.dart';
-
+import 'package:http/http.dart' as http;
 
 class SignUpForm extends StatefulWidget {
   @override
@@ -14,24 +251,99 @@ class SignUpForm extends StatefulWidget {
 
 class _SignUpFormState extends State<SignUpForm> {
   final _formKey = GlobalKey<FormState>();
-  String? Name;
+  String? name;
   String? email;
   String? number;
   bool remember = false;
-  final List<String?> errors = [];
+  final List<String> errors = [];
 
-  void addError({String? error}) {
-    if (!errors.contains(error))
+  void addError({required String error}) {
+    if (!errors.contains(error)) {
       setState(() {
         errors.add(error);
       });
+    }
   }
 
-  void removeError({String? error}) {
-    if (errors.contains(error))
+  void removeError({required String error}) {
+    if (errors.contains(error)) {
       setState(() {
         errors.remove(error);
       });
+    }
+  }
+
+  Future<void> handleSignup() async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://172.26.192.1:3000/signup'),
+        body: {
+          'name': name,
+          'email': email,
+          'phone': number,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        // Signup successful, show the welcome message
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Success'),
+              content: Text('Signup successful. Welcome email sent!'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    // Navigate to another screen or perform necessary actions
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        // Signup failed, handle the error
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Error'),
+              content: Text(response.body),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (error) {
+      // Handle any network or server errors
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Signup error: $error'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
@@ -49,11 +361,12 @@ class _SignUpFormState extends State<SignUpForm> {
           FormError(errors: errors),
           SizedBox(height: getProportionateScreenHeight(20)),
           DefaultButton(
-            text: "Continue",
+            text: "SIGNUP",
             press: () {
               if (_formKey.currentState!.validate()) {
                 _formKey.currentState!.save();
-                // if all are valid then go to success screen
+                handleSignup();
+                // If all are valid, go to the success screen
                 // Navigator.pushNamed(context, CompleteProfileScreen.routeName);
               }
             },
@@ -63,110 +376,16 @@ class _SignUpFormState extends State<SignUpForm> {
     );
   }
 
-  // TextFormField buildConformPassFormField() {
-  //   return TextFormField(
-  //     obscureText: true,
-  //     onSaved: (newValue) => conform_password = newValue,
-  //     onChanged: (value) {
-  //       if (value.isNotEmpty) {
-  //         removeError(error: kPassNullError);
-  //       } else if (value.isNotEmpty && password == conform_password) {
-  //         removeError(error: kMatchPassError);
-  //       }
-  //       conform_password = value;
-  //     },
-  //     validator: (value) {
-  //       if (value!.isEmpty) {
-  //         addError(error: kPassNullError);
-  //         return "";
-  //       } else if ((password != value)) {
-  //         addError(error: kMatchPassError);
-  //         return "";
-  //       }
-  //       return null;
-  //     },
-  //     decoration: InputDecoration(
-
-  //       hintText: "Re-enter your password",
-  //       border: OutlineInputBorder(
-  //         borderRadius: BorderRadius.circular(5.0),
-  //       ),
-  //       contentPadding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
-  //       // If  you are using latest version of flutter then lable text and hint text shown like this
-  //       // if you r using flutter less then 1.20.* then maybe this is not working properly
-  //       floatingLabelBehavior: FloatingLabelBehavior.always,
-  //       suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
-  //     ),
-  //   );
-  // }
-
-  // TextFormField buildPasswordFormField() {
-  //   return TextFormField(
-  //     obscureText: true,
-  //     onSaved: (newValue) => password = newValue,
-  //     onChanged: (value) {
-  //       if (value.isNotEmpty) {
-  //         removeError(error: kPassNullError);
-  //       } else if (value.length >= 8) {
-  //         removeError(error: kShortPassError);
-  //       }
-  //       password = value;
-  //     },
-  //     validator: (value) {
-  //       if (value!.isEmpty) {
-  //         addError(error: kPassNullError);
-  //         return "";
-  //       } else if (value.length < 8) {
-  //         addError(error: kShortPassError);
-  //         return "";
-  //       }
-  //       return null;
-  //     },
-  //     decoration: InputDecoration(
-  //       hintText: "Enter your password",
-  //       border: OutlineInputBorder(
-  //         borderRadius: BorderRadius.circular(5.0),
-  //       ),
-  //       contentPadding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
-  //       // If  you are using latest version of flutter then lable text and hint text shown like this
-  //       // if you r using flutter less then 1.20.* then maybe this is not working properly
-  //       floatingLabelBehavior: FloatingLabelBehavior.always,
-  //       suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
-  //     ),
-  //   );
-  // }
-
   TextFormField buildNameFormField() {
     return TextFormField(
       keyboardType: TextInputType.name,
-      onSaved: (newValue) => Name = newValue,
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kEmailNullError);
-        } else if (emailValidatorRegExp.hasMatch(value)) {
-          removeError(error: kInvalidEmailError);
-        }
-        return null;
-      },
-      validator: (value) {
-        if (value!.isEmpty) {
-          addError(error: kEmailNullError);
-          return "";
-        } else if (!emailValidatorRegExp.hasMatch(value)) {
-          addError(error: kInvalidEmailError);
-          return "";
-        }
-        return null;
-      },
+      onSaved: (newValue) => name = newValue,
       decoration: InputDecoration(
-
         hintText: "Enter Your Name",
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5.0),
         ),
-        contentPadding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
-        // If  you are using latest version of flutter then lable text and hint text shown like this
-        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
         floatingLabelBehavior: FloatingLabelBehavior.always,
         suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Name.svg"),
       ),
@@ -183,7 +402,6 @@ class _SignUpFormState extends State<SignUpForm> {
         } else if (emailValidatorRegExp.hasMatch(value)) {
           removeError(error: kInvalidEmailError);
         }
-        return null;
       },
       validator: (value) {
         if (value!.isEmpty) {
@@ -196,14 +414,11 @@ class _SignUpFormState extends State<SignUpForm> {
         return null;
       },
       decoration: InputDecoration(
-
         hintText: "Enter Your Email Address",
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5.0),
         ),
-        contentPadding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
-        // If  you are using latest version of flutter then lable text and hint text shown like this
-        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
         floatingLabelBehavior: FloatingLabelBehavior.always,
         suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
       ),
@@ -216,11 +431,10 @@ class _SignUpFormState extends State<SignUpForm> {
       onSaved: (newValue) => number = newValue,
       onChanged: (value) {
         if (value.isNotEmpty) {
-          removeError(error: kEmailNullError);
+          removeError(error: kPhoneNumberNullError);
         } else if (phoneValidatorRegExp.hasMatch(value)) {
           removeError(error: kInvalidPhoneError);
         }
-        return null;
       },
       validator: (value) {
         if (value!.isEmpty) {
@@ -233,16 +447,13 @@ class _SignUpFormState extends State<SignUpForm> {
         return null;
       },
       decoration: InputDecoration(
-
         hintText: "Enter Your Phone Number",
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5.0),
         ),
-        contentPadding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
-        // If  you are using latest version of flutter then lable text and hint text shown like this
-        // if you r using flutter less then 1.20.* then maybe this is not working properly
+        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
         floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Phone.svg"),
       ),
     );
   }
