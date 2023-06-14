fb8adf9a0b6b882e93e510703cd0de174e6b8d95
Mirza Rizwan <Mirza.rizwan0311@gmail.com>
2023-06-14 15:34:29 +0500

    FIXING UI IN PRODUCT.DART V0.0.1b

--- lib/screens/home/components/product.dart
+++ lib/screens/home/components/product.dart
@@ -1,6 +1,7 @@
-import '../../../screens/home/components/MeasurementProfile.dart';
+// import '../../../screens/home/components/MeasurementProfile.dart';
 import 'package:flutter/material.dart';
-import '../components/MeasurementPopup.dart';
+import '../../../size_config.dart';
+// import '../components/MeasurementPopup.dart';
 
 // void main() => runApp(ProductPage());
 
@@ -113,7 +114,7 @@ class _ProductPageState extends State<ProductPage> {
                         Text(
                           'Neck Styles',
                           style: TextStyle(
-                            fontSize: 24,
+                            fontSize: 21,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -184,7 +185,7 @@ class _ProductPageState extends State<ProductPage> {
                         Text(
                           'Sleeves Styles',
                           style: TextStyle(
-                            fontSize: 24,
+                            fontSize: 21,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -255,7 +256,7 @@ class _ProductPageState extends State<ProductPage> {
                         Text(
                           'Trouser Length',
                           style: TextStyle(
-                            fontSize: 24,
+                            fontSize: 21,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -342,7 +343,71 @@ class _ProductPageState extends State<ProductPage> {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.pushNamed(context, MeasurementPopup.routeName);
+                    showDialog(
+
+                      context: context,
+                      builder: (BuildContext context) {
+                        String selectedProfile = ''; // Variable to store the selected profile
+                        final Function(String) onProfileSelected;
+                        return AlertDialog(
+                          title: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('Measurement Popup'),
+                                  IconButton(
+                                    icon: Icon(Icons.close),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 12.0),
+                            ],
+                          ),
+                          content: MeasurementPopup(onProfileSelected: (profile) {
+                            selectedProfile = profile;
+                          }),
+                          actions: [
+                            Align(
+                              alignment: Alignment.center,
+                              child: TextButton(
+                                onPressed: () {
+                                  if (selectedProfile.isEmpty) {
+                                    Navigator.of(context).pop();
+                                  } else {
+                                    showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: Text('Measurement Profile Required'),
+                                          content: Text('Please select a measurement profile.'),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text('OK'),
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  }
+                                },
+
+                                child: Text('Continue'),
+                              ),
+                            ),
+                          ],
+                        );
+
+
+
+                      },
+                    );
                   },
                   child: Text('Add to Cart'),
                 ),
@@ -419,6 +484,217 @@ class SleevesCard extends StatelessWidget {
               offset: Offset(0, 3),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class MeasurementPopup extends StatefulWidget {
+
+  MeasurementPopup({required this.onProfileSelected});
+  final Function(String) onProfileSelected;
+
+  @override
+  _MeasurementPopupState createState() => _MeasurementPopupState();
+}
+
+class _MeasurementPopupState extends State<MeasurementPopup> {
+  List<String> measurementProfiles = ['Profile 1', 'Profile 2'];
+  String? selectedProfile;
+  String selectedPreset = '';
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        DropdownButton<String>(
+          value: selectedProfile,
+          hint: Text('Select Profile'),
+          onChanged: (value) {
+            setState(() {
+              selectedProfile = value;
+            });
+          },
+          items: measurementProfiles.map((profile) {
+            return DropdownMenuItem<String>(
+              value: profile,
+              child: Text(profile),
+            );
+          }).toList(),
+        ),
+        SizedBox(height: 12),
+        Text('Or'),
+        SizedBox(height: 12),
+        ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('New Measurement Profile'),
+                  content: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter Your Measurement Profile name',
+                          ),
+                          onChanged: (value) {
+                            // Handle profile name change
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        Text(
+                          'Enter Your Body Measurements for a Swift Experience',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 12.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter chest measurement',
+                          ),
+                          onChanged: (value) {
+                            // Handle chest measurement change
+                          },
+                        ),
+                        SizedBox(height: 12.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter waist measurement',
+                          ),
+                          onChanged: (value) {
+                            // Handle waist measurement change
+                          },
+                        ),
+                        SizedBox(height: 12.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter hips measurement',
+                          ),
+                          onChanged: (value) {
+                            // Handle hips measurement change
+                          },
+                        ),
+                        SizedBox(height: 12.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter inseam measurement',
+                          ),
+                          onChanged: (value) {
+                            // Handle inseam measurement change
+                          },
+                        ),
+                        SizedBox(height: 12.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter sleeve measurement',
+                          ),
+                          onChanged: (value) {
+                            // Handle sleeve measurement change
+                          },
+                        ),
+                        SizedBox(height: 12.0),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter shoulder measurement',
+                          ),
+                          onChanged: (value) {
+                            // Handle shoulder measurement change
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Handle create new profile
+                        // Example: Save the new profile and close the popup
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Save'),
+                    ),
+                  ],
+
+                );
+              },
+            );
+          },
+          child: Text('Create New Profile'),
+        ),
+      ],
+    );
+  }
+}
+
+class MeasurementInputField extends StatelessWidget {
+
+  final String hintText;
+
+  const MeasurementInputField({
+    Key? key,
+
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+
+        hintText: hintText,
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.symmetric(horizontal: 12.0,vertical: 12.0),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'This field is required';
+        }
+        return null;
+      },
+    );
+  }
+}
+class PresetSizeButton extends StatelessWidget {
+  final String size;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const PresetSizeButton({
+    Key? key,
+    required this.size,
+    required this.isSelected,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.amber : Color(0xFF774AC7),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: Text(
+          size,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
         ),
       ),
     );
